5e5abca814440baf9c74977d7411e99f83f078ae
alkeal <yhma22.alkeal@folkuniversitetet.nu>
2023-10-12 15:16:27 +0200

    Fix

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,17 +1,26 @@
 
 import {Link} from "react-router-dom";
 import React from "react";
+import BackgroundImage from  "../assets/homeBackground.jpeg"
+
 
 function home(){
 
+   
     return (
+        <div className="header">
 
 
-        <h1>FUNKA</h1>
-
+      
+              <h1 className="title">
+   
+                  WELCOME TO
+                  <span className="color">FILMHOUND</span></h1>
+              <button className="browse-button">BROWSE</button>
+              <h2 className="title1">PICK A <span className="color2">MOVIE</span></h2>
+              </div>
 
     )
-
 
 
 
