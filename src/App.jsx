1f36c46e3e3f77b1cda4f29b7c6c8d1ac91c1fa4
alkeal <yhma22.alkeal@folkuniversitetet.nu>
2023-10-12 13:33:29 +0200

    API moved

--- src/App.jsx
+++ src/App.jsx
@@ -8,51 +8,11 @@ import Checkout from "./components/checkout"
 import {BrowserRouter as Router, Route, Routes}from "react-router-dom"
 
 
-const API_KEY = "973b4444";
-const BASE_URL = "http://www.omdbapi.com/?apikey=";
-const URL = `${BASE_URL}${API_KEY}`;
-const TITLE_SEARCH_PARAM = "&s="; // https://www.omdbapi.com/ API parameters
 
-console.log(`API URL: ${URL}`);
-
-function App() {
-  const [title, setTitle] = useState("");
-  const [filmData, setFilmData] = useState(null);
-
-  const handleInput = (input) => {
-    setTitle(input.target.value);
-  }
-
-  const getFilm = async () => {
-    const searchByTitleUrl = URL + TITLE_SEARCH_PARAM;
-  
-    const response = await fetch(searchByTitleUrl + title);
-    const filmData = await response.json();
-  
-    console.log(filmData);
-    setFilmData(filmData);
-  }
-  
+export default function App(){
 
 
-
-  return (
-    <div>
-      <h1>Film Hound</h1>
-      <input type="text" value={title} onChange={handleInput} />
-      <button onClick={getFilm}>Search</button>
-      <button>Browse</button>
-      {filmData && <SearchedFilms filmData={filmData} />}
-    </div>
-  )
-}
-
-export default App;
-
-/*---------------------------/*
-// Tänkte att detta skulle vara det ända på denna fil så själva grunden
- hur varje sida ska se ut finns . ..
-
+return (
 <div className="app">
    <Router>
 
@@ -73,4 +33,5 @@ export default App;
 
 </Router>
 </div>
-/*---------------------------*/
+)
+}
