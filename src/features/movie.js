e09296624f37f9db062675d7ab38db9a1b1dc34c
Antonio Saracino <antonio.saracino83@gmail.com>
2023-10-12 18:22:38 +0200

    Redux structure initiated.

---
+++ src/features/movie.js
@@ -0,0 +1,24 @@
+import { createAction, createReducer } from "@reduxjs/toolkit";
+
+const add = createAction("add movie to cart");
+const remove = createAction("remove movie from cart");
+
+const actions = { add, remove };
+
+const initialState = {
+  cart: [],
+};
+
+// Payload will represent the movie details
+
+const reducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(add, (state, action) => {
+      state.cart.push(action.payload);
+    })
+    .addCase(remove, (state, action) => {
+      state.cart = state.cart.filter((item = item.id !== action.payload.id));
+    });
+});
+
+export default { reducer, actions };
