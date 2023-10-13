2ae1e0b357c2ac6d3592fad2f4f86ef75e79349b
alkeal <yhma22.alkeal@folkuniversitetet.nu>
2023-10-13 15:12:47 +0200

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,11 @@ import "./index.css";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 
-const store = configureStore({
-  reducer: rootReducer,
-});
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
+    
   </React.StrictMode>
 );
