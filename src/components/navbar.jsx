78dc2991d9547528d90de1cea220e9dd50caea1e
Marlon <marlongarciab@hotmail.com>
2023-10-16 12:18:21 +0200

    ww

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,6 @@
-import Logo from '/src/assets/Logo.png'
 import HomeIcon from '/src/assets/home_Icon.png'
 import SearchIcon from '/src/assets/icon_search.png'
 import ShoppingIcon from '/src/assets/icon_shopping.png'
-
 import {Link} from "react-router-dom"
 import React, { useState } from 'react'
 import './navbar.css'
@@ -13,21 +11,16 @@ export default function Navbar (){
 const [openLinks, setOpenLinks] = useState(false)
 
 
-
 return (
 
 
 <div className="navbar">
 <div className="leftSide" id={openLinks ? "open" : "close"}>
- <img src= {Logo}/>
+ <h1 className='logo'>FILM<span className='blue'>HOUND</span></h1>
  <div className="hiddenLinks">
 
 
-
  </div>
-
-
-
 </div>
 <div className="rightSide">
 
