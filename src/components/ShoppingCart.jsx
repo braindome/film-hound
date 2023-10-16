ffb91a2d24e129f598a3c6681d769417721d9c32
Antonio Saracino <antonio.saracino83@gmail.com>
2023-10-16 16:04:18 +0200

    Shopping Cart styled

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import CartItem from "./CartItem";
+import "./styles/ShoppingCart.css"
 
 const ShoppingCart = () => {
   const cartItems = useSelector((state) => state.cart);
+  const total = cartItems.length * 39;
 
   return (
     <div className="cart_wrapper">
-      <h2>Cart</h2>
+      <h2>Shopping Cart</h2>
       {cartItems.map((film, index) => (
         <CartItem key={index} film={film} />
       ))}
+      <h4>Total items: {cartItems.length} </h4>
+      <div className="checkout_row" >
+        <h3>Total: {total} kr </h3>
+        <button>Checkout</button>
+      </div>
+
     </div>
   );
 };
