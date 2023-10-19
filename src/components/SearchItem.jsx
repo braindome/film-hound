0a29e09e768e0b467bdc7b578af65ef0bb19b382
Antonio Saracino <antonio.saracino83@gmail.com>
2023-10-19 13:06:59 +0200

    Began working on the styling

--- src/components/SearchItem.jsx
+++ src/components/SearchItem.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import fallback from "/src/assets/NoImage.png";
+import "./styles/SearchItem.css"
 
 const SearchItem = ({ film, handleBuy, getFilmDetails }) => {
   const [expanded, setExpanded] = useState(false);
@@ -31,59 +32,110 @@ const SearchItem = ({ film, handleBuy, getFilmDetails }) => {
   }, [expanded, film.imdbID, getFilmDetails]);
 
   return (
-    <div>
-      <div className="posterContainer">
-        <img
-          className="poster"
-          src={film.Poster}
-          onError={(e) => (e.currentTarget.src = fallback)}
-          alt={film.Title}
-        />
-      </div>
 
-      <div>
-        <h3 className="filmTitle">{film.Title}</h3>
-        <p>Type: {film.Type}</p>
-        <p>Year: {film.Year}</p>
-        <p>IMDB ID: {film.imdbID}</p>
-        <a href={`http://www.imdb.com/title/${film.imdbID}`}>IMDB Page</a>
-      </div>
 
-      <div>
-        <button className="buyMovieBtn" onClick={() => handleBuy(film)}>
-          Buy
-        </button>
-        <button
-          className="more-info-btn" onClick={toggleExpanded}
-        //   onClick={() => {
-        //     setExpanded(!expanded);
-        //     getFilmDetails(film.imdbID);
-        //     setSelectedFilm(film);
-        //   }}
-        
-        >
-          {expanded ? "LESS" : "MORE"}
-        </button>
-        {expanded && (
-          <section className="expanded-info">
-            <section className="data">
-              {fullFilmData ? (
-                <div>
-                  <h3>Runtime: {fullFilmData.Runtime}</h3>
-                  <h3>Director: {fullFilmData.Director}</h3>
-                  <h4>Released: {fullFilmData.Released} </h4>
-                  <p>Plot: {fullFilmData.Plot}</p>
-                  <h4>IMDB Rating: {fullFilmData.imdbRating} </h4>
-                  <h4>Metascore: {fullFilmData.Metascore} </h4>
-                </div>
-              ) : (
-                <p>Loading film details...</p>
-              )}
-            </section>
-          </section>
-        )}
-      </div>
+<div className={`search-item-container ${expanded ? 'expanded' : ''}`} >
+
+  <div className="title-poster" >
+    <h3 className="film-title">{film.Title}</h3>
+    <div className="year-type-row">
+      <p>Type: {film.Type}</p>
+      <p>Year: {film.Year}</p>
     </div>
+  </div>
+
+  <div className="poster">
+    <img
+      src={film.Poster}
+      onError={(e) => (e.currentTarget.src = fallback)}
+      alt={film.Title}
+    />
+  </div>
+
+  <div className="buttons-row">
+    <button className="buyMovieBtn" onClick={() => handleBuy(film)}>Buy</button>
+    <button className="more-info-btn" onClick={toggleExpanded}>{expanded ? "Less" : "More"}</button>
+  </div>
+
+  <div className="expanded_info_wrapper">
+    {expanded && (
+      <section className="expanded-info">
+        <section className="data">
+          {fullFilmData ? (
+            <section>
+              <p className="plot">Plot: {fullFilmData.Plot}</p>
+              <div className="detailed-info-bottom-container" >
+                <p>Runtime: {fullFilmData.Runtime}</p>						
+                <p>Director: {fullFilmData.Director}</p>							
+                <p>Released: {fullFilmData.Released} </p>						
+                <p>IMDB Rating: {fullFilmData.imdbRating} </p>						
+                <p>Metascore: {fullFilmData.Metascore} </p>						
+                <a href={`http://www.imdb.com/title/${film.imdbID}`}><p>IMDB</p></a>
+              </div>
+            </section>
+          ) : (
+            <p>Loading film details...</p>
+          )}
+        </section>
+      </section>
+    )}
+
+  </div>
+
+</div>
+    // <div>
+    //   <div className="posterContainer">
+    //     <img
+    //       className="poster"
+    //       src={film.Poster}
+    //       onError={(e) => (e.currentTarget.src = fallback)}
+    //       alt={film.Title}
+    //     />
+    //   </div>
+
+    //   <div>
+    //     <h3 className="filmTitle">{film.Title}</h3>
+    //     <p>Type: {film.Type}</p>
+    //     <p>Year: {film.Year}</p>
+    //     <p>IMDB ID: {film.imdbID}</p>
+    //     <a href={`http://www.imdb.com/title/${film.imdbID}`}>IMDB Page</a>
+    //   </div>
+
+    //   <div>
+    //     <button className="buyMovieBtn" onClick={() => handleBuy(film)}>
+    //       Buy
+    //     </button>
+    //     <button
+    //       className="more-info-btn" onClick={toggleExpanded}
+    //     //   onClick={() => {
+    //     //     setExpanded(!expanded);
+    //     //     getFilmDetails(film.imdbID);
+    //     //     setSelectedFilm(film);
+    //     //   }}
+        
+    //     >
+    //       {expanded ? "LESS" : "MORE"}
+    //     </button>
+    //     {expanded && (
+    //       <section className="expanded-info">
+    //         <section className="data">
+    //           {fullFilmData ? (
+    //             <div>
+    //               <h3>Runtime: {fullFilmData.Runtime}</h3>
+    //               <h3>Director: {fullFilmData.Director}</h3>
+    //               <h4>Released: {fullFilmData.Released} </h4>
+    //               <p>Plot: {fullFilmData.Plot}</p>
+    //               <h4>IMDB Rating: {fullFilmData.imdbRating} </h4>
+    //               <h4>Metascore: {fullFilmData.Metascore} </h4>
+    //             </div>
+    //           ) : (
+    //             <p>Loading film details...</p>
+    //           )}
+    //         </section>
+    //       </section>
+    //     )}
+    //   </div>
+    // </div>
   );
 };
 
